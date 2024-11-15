4398ed2bfbc129d2a2c51d8798ddd199cab5463d
ravilella1234 <masters.selenium@gmail.com>
2024-11-15 18:14:44 +0530

    Java Interface & Abstract Class

---
+++ NareshIT5PMNovemberBatch/src/icici/loans/carloans/HDFC.java
@@ -0,0 +1,22 @@
+package icici.loans.carloans;
+
+public class HDFC implements Rbi
+{
+
+	@Override
+	public void deposit() {
+		System.out.println("Iam overriden deposit in HDFC");
+	}
+
+	@Override
+	public void withdrawl() {
+		System.out.println("Iam overriden withdrawl in HDFC");
+	}
+	
+	public static void main(String[] args) {
+		HDFC h = new HDFC();
+		h.deposit();
+		h.withdrawl();
+	}
+
+}
---
+++ NareshIT5PMNovemberBatch/src/icici/loans/carloans/Rbi.java
@@ -0,0 +1,14 @@
+package icici.loans.carloans;
+
+public interface Rbi 
+{
+	int x = 10;
+	
+	public void deposit();
+	public void withdrawl();
+	
+	public static void main(String[] args) 
+	{
+		//Rbi i = new Rbi();
+	}
+}
