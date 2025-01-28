dfa8ffa81d8e13607f1a1f5ad80d89f9b6e95b1e
ravilella1234 <ravilella.1981@gmail.com>
2025-01-28 18:20:50 +0530

    Locator day-2

--- SeleniumAutomation5PMBatch/src/test/java/com/pack/BaseTest.java
+++ SeleniumAutomation5PMBatch/src/test/java/com/pack/BaseTest.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -18,6 +20,7 @@ public class BaseTest
 	public static Properties p;
 	public static Properties mainprop;
 	public static Properties childprop;
+	public static Properties orprop;
 	
 	public static void init() throws Exception
 	{
@@ -34,11 +37,17 @@ public class BaseTest
 		String e = mainprop.getProperty("env");
 		System.out.println(e);
 		
+		
+		
 		fis = new FileInputStream(projectPath+"/src/test/resource/"+e+".properties");
 		childprop = new Properties();
 		childprop.load(fis);
 		String url = childprop.getProperty("amazonurl");
 		System.out.println(url);
+		
+		fis = new FileInputStream(projectPath+"/src/test/resource/or.properties");
+		orprop = new Properties();
+		orprop.load(fis);
 	}
 	
 	public static void launch(String browser)
@@ -50,11 +59,53 @@ public class BaseTest
 		}else {
 			driver = new EdgeDriver();
 		}
+		
+		driver.manage().window().maximize();
 	}
 	
 	public static void navigateUrl(String url)
 	{
 		driver.navigate().to(childprop.getProperty(url));
+	}
+	
+	public static void clickElement(String locatorkey) 
+	{
+		getLocatorType(locatorkey).click();
+		//driver.findElement(By.xpath(locator)).click();
+	}
+
+	public static WebElement getLocatorType(String locatorkey) 
+	{
+		WebElement element = null;
+		
+		if(locatorkey.endsWith("_id")) {
+			element = driver.findElement(By.id(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("name")) {
+			element = driver.findElement(By.name(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("classname")) {
+			element = driver.findElement(By.className(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("xpath")) {
+			element = driver.findElement(By.xpath(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("css")) {
+			element = driver.findElement(By.cssSelector(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("linktext")) {
+			element = driver.findElement(By.linkText(orprop.getProperty(locatorkey)));
+		}else if (locatorkey.endsWith("partiallinktext")) {
+			element = driver.findElement(By.partialLinkText(orprop.getProperty(locatorkey)));
+		}
+		return element;
+	}
+
+	public static void typeText(String locatorkey, String text) 
+	{
+		getLocatorType(locatorkey).sendKeys(text);
+		//driver.findElement(By.name(locator)).sendKeys(text);
+	}
+
+	public static void selectOption(String locatorkey, String option) 
+	{
+		getLocatorType(locatorkey).sendKeys(option);
+		//driver.findElement(By.id(locator)).sendKeys(option);
 	}
 
 }
--- SeleniumAutomation5PMBatch/src/test/java/com/pack/TC_002.java
+++ SeleniumAutomation5PMBatch/src/test/java/com/pack/TC_002.java
@@ -1,5 +1,7 @@
 package com.pack;
 
+import org.openqa.selenium.By;
+
 public class TC_002 extends BaseTest
 {
 
@@ -12,5 +14,21 @@ public class TC_002 extends BaseTest
 		
 		navigateUrl("amazonurl");
 		
+		Thread.sleep(10000);
+		
+		selectOption("amazondropbox_id","Books");
+		
+		typeText("amazonsearchbox_id","Harry Potter");
+		
+		clickElement("amazonsearchbutton_xpath");
+		
+		
+		//driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
+		
+		//driver.findElement(By.name("field-keywords")).sendKeys("Harry Potter");
+		
+		//driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
 	}
+
+	
 }
