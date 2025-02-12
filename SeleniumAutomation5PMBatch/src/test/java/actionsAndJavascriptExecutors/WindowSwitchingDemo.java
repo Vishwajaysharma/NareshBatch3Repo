e681958b03c235656c780e7ca83ade24277cae58
ravilella1234 <ravilella.1981@gmail.com>
2025-02-12 18:41:55 +0530

    avascriptExecutor & WindowSwitching

---
+++ SeleniumAutomation5PMBatch/src/test/java/actionsAndJavascriptExecutors/WindowSwitchingDemo.java
@@ -0,0 +1,24 @@
+package actionsAndJavascriptExecutors;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowSwitchingDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.switchTo().newWindow(WindowType.TAB);
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		driver.get("https://www.redbus.in/");
+		System.out.println(driver.getTitle());
+	}
+
+}
