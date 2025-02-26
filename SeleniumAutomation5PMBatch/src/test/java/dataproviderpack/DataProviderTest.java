c2239b3247ae96e6d42d3fc040abe1272657ee93
ravilella1234 <ravilella.1981@gmail.com>
2025-02-26 18:29:36 +0530

    testNG Day-11

---
+++ SeleniumAutomation5PMBatch/src/test/java/dataproviderpack/DataProviderTest.java
@@ -0,0 +1,17 @@
+package dataproviderpack;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderTest 
+{
+	
+  @DataProvider() //name="data"
+  public Object[][] data() 
+  {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+      new Object[] { 3, "c" }
+    };
+  }
+}
