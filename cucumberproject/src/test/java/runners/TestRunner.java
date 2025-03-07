04c1823c1785420437ac5f3f5b5169c37afd06b5
ravilella1234 <ravilella.1981@gmail.com>
2025-03-07 18:06:08 +0530

    BDD Cucumber Day-8

--- cucumberproject/src/test/java/runners/TestRunner.java
+++ cucumberproject/src/test/java/runners/TestRunner.java
@@ -1,20 +1,32 @@
 package runners;
 
+
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-					features = "src/test/resources/feature",
+					features = "src/test/resources/feature/logindemo.feature",
 					glue = {"steps","hooks"},
 					plugin = {
 							  "pretty",
 							  "html:target/cucumber-report.html"
-							  }
+							  },
+					publish = true
 				)
-public class TestRunner 
-{
 
+public class TestRunner extends AbstractTestNGCucumberTests
+{
+	@DataProvider(parallel = true)
+	public Object[][] scenarios()
+	{
+		return super.scenarios();
+	}
 }
